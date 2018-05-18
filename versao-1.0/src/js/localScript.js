a3eafc01d0d677740aa748995d0fa2b904039439
adrianobaz <adrianobraga.az@Hotmail.com>
2018-05-17 21:19:23 -0300

    Primeiro commit

---
+++ versao-1.0/src/js/localScript.js
@@ -0,0 +1,29 @@
+//
+$(".map-icon").click(function() {
+            $(".map-fix").toggle();
+        });
+
+
+// Want to customize colors? go to snazzymaps.com
+        function myMap() {
+            var maplat = $('#map').data('lat');
+            var maplon = $('#map').data('lon');
+            var mapzoom = $('#map').data('zoom');
+            // Styles a map in night mode.
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: {
+                    lat: maplat,
+                    lng: maplon
+                },
+                zoom: mapzoom,
+                scrollwheel: false
+            });
+            var marker = new google.maps.Marker({
+                position: {
+                    lat: maplat,
+                    lng: maplon
+                },
+                map: map,
+                title: 'We are here!'
+            });
+        }
